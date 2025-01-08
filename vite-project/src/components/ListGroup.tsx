cbac419a6e94e0a9fcec1d5c2772c9ea6949bf42
Nisharg Modi <n.modi2874@gmail.com>
2025-01-08 22:51:24 +0530

    Last commit Day 1

--- vite-project/src/components/ListGroup.tsx
+++ vite-project/src/components/ListGroup.tsx
@@ -1,6 +1,39 @@
 
 function ListGroup() {
-    return (<h1>Do I Wanna Know?</h1>)
+    let items = [
+        'Nisharg',
+        'Kkavy',
+        'Minku',
+        'Ayush',
+        'Jeetu',
+        'Mannu']
+
+    items = [];
+    // if(items.length === 0) {
+    //     return (<>
+    //     <h1>List Group</h1>
+    //     <h6>No Items found LOL !!!</h6>
+    //     </>)
+    // }
+    const message = items.length === 0 ? <p>No Items Found</p> : null
+    return (
+        <>
+        <h1>Homies</h1>
+        {/* <ul className="list-group">
+            <li className="list-group-item">Item 1</li>
+            <li className="list-group-item">Item 2</li>
+            <li className="list-group-item">Item 3</li>
+            <li className="list-group-item">Item 4</li>
+            <li className="list-group-item">Item 5</li>
+        </ul> */}
+        {message}
+        <ul className="list-group">
+            {items.map((item) => (
+                <li className="list-group-item" key={item}>{item}</li>
+            ))}
+        </ul>
+        </>
+    )
 }
 
 export default ListGroup

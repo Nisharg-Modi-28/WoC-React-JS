2721e8a5a0ee3dd3cad954421b3dba7e9c1eceac
Nisharg Modi <n.modi2874@gmail.com>
2025-01-10 21:38:05 +0530

    basic button set-up

--- counter-app/src/App.tsx
+++ counter-app/src/App.tsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Button from 'react-bootstrap/Button';
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
+function counter_app() {
+  let counter = 0;
+  return(
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Button type='reset' onClick={() => {
+        counter=0;
+        console.log(`value of counter var is ${counter}`);
+      }}>Reset</Button>
+      <Button type='reset' 
+      onClick={() => {
+        counter++;
+        console.log(`value of counter var is ${counter}`);
+      }}>+</Button>
+      <Button type='reset' 
+      onClick={() => {
+        counter--;
+        console.log(`value of counter var is ${counter}`);
+      }}
+      >-</Button>
+      <Button onClick={}>{counter}</Button>
     </>
   )
 }
 
-export default App
+export default counter_app

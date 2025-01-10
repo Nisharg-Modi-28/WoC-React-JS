10db0ec4342c438be7e5e88b79ea332f4536619b
Nisharg Modi <n.modi2874@gmail.com>
2025-01-10 22:43:26 +0530

    implementing with help of usestate

--- counter-app/src/App.tsx
+++ counter-app/src/App.tsx
@@ -1,10 +1,12 @@
-import Button from 'react-bootstrap/Button';
+// import Button from 'react-bootstrap/Button';
+import { useState, CSSProperties } from 'react';
+import { Button } from 'react-bootstrap';
 
 function counter_app() {
-  let counter = 0;
+  const [counter , setCounter] = useState(0);
   return(
     <>
-      <Button type='reset' onClick={() => {
+      {/* <Button type='reset' onClick={() => {
         counter=0;
         console.log(`value of counter var is ${counter}`);
       }}>Reset</Button>
@@ -19,9 +21,32 @@ function counter_app() {
         console.log(`value of counter var is ${counter}`);
       }}
       >-</Button>
-      <Button onClick={}>{counter}</Button>
+      <Button onClick={() => {}}>{counter}</Button> */}
+      <div style={styles.container}>
+        <Button 
+        style = {styles.counter}
+        onClick={(counter) => {
+          // setCounter(counter+1)
+        }}>Add</Button>
+      </div>
     </>
   )
 }
 
+const styles: { [key: string]: CSSProperties } = {
+  container: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: '100vh',
+  },
+  button: {
+    margin: '5px',
+  },
+  counter: {
+    fontSize: '2rem',
+    margin: '10px',
+  },
+};
 export default counter_app

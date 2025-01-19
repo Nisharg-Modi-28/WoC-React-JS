3ac3d3b81c9916ff8a3ee9b3612006ebd6cd6d75
Nisharg Modi <n.modi2874@gmail.com>
2025-01-19 16:04:52 +0530

    Completed this App

--- To-Do List-JS/script.js
+++ To-Do List-JS/script.js
@@ -2,12 +2,39 @@ const input_box = document.getElementById("task");
 const taskList = document.getElementById('taskList');
 
 function addTask() {
-    if(input_box === '') {
+    if(input_box.value === '') {
         alert("Write something !");
     }
     else {
         let li = document.createElement("li");
         li.innerHTML = input_box.value;
         taskList.appendChild(li);
+
+        let span = document.createElement('span');
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
     }
+    input_box.value = '';
+    saveData();
 }
+
+taskList.addEventListener("click",function(e){
+    if(e.target.tagName === "LI") {
+        e.target.classList.toggle("checked");
+        saveData();
+    }
+    else if(e.target.tagName === "SPAN") {
+        e.target.parentElement.remove();
+        saveData();
+    }
+},false);
+
+function saveData() {
+    localStorage.setItem("data",taskList.innerHTML);
+}
+
+function showData() {
+    taskList.innerHTML = localStorage.getItem("data");
+}
+
+showData();
